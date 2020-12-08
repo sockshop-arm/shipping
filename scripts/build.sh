7fe82b4fa81ac0c18f75bd625976736ce0aab14f
root <root@raspberrypi>
2020-12-08 06:02:24 -0300

    Remove zipkin, use ARM compatible maven and fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,27 +4,16 @@ set -ev
 
 export BUILD_VERSION="0.0.2-SNAPSHOT"
 export BUILD_DATE=`date +%Y-%m-%dT%T%z`
+GROUP=codelab
+COMMIT=latest
 
 SCRIPT_DIR=$(dirname "$0")
 
-if [[ -z "$GROUP" ]] ; then
-    echo "Cannot find GROUP env var"
-    exit 1
-fi
+DOCKER_CMD=docker
 
-if [[ -z "$COMMIT" ]] ; then
-    echo "Cannot find COMMIT env var"
-    exit 1
-fi
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    DOCKER_CMD=docker
-else
-    DOCKER_CMD="sudo docker"
-fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
-$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
+$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven paperinik/rpi-maven:3.6.3 install -DskipTests package
 
 cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
 
